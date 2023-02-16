86a8a6e858f0a569125b0cc1b4fdcbd8ba7f4716
Marzena <git@marzka.cafe>
2023-02-16 21:30:57 +0000

    fix migration

--- kulupulang/migrations/0012_alter_user_theme.py
+++ kulupulang/migrations/0012_alter_user_theme.py
@@ -4,16 +4,34 @@ from django.db import migrations, models
 import django.db.models.deletion
 
 
-class Migration(migrations.Migration):
+def convert_theme(apps, schema_editor):
+    User = apps.get_model("kulupulang", "User")
+    Theme = apps.get_model("kulupulang", "Theme")
+    for user in User.objects.all():
+        if user.theme:
+            try:
+                user.theme = Theme.objects.get(name=user.theme).id
+                user.save()
+            except Theme.DoesNotExist:
+                user.theme = 1
+                user.save()
 
+
+class Migration(migrations.Migration):
     dependencies = [
-        ('kulupulang', '0011_theme'),
+        ("kulupulang", "0011_theme"),
     ]
 
     operations = [
+        migrations.RunPython(convert_theme, reverse_code=migrations.RunPython.noop),
         migrations.AlterField(
-            model_name='user',
-            name='theme',
-            field=models.ForeignKey(default=1, help_text='the theme you want to display the site in', on_delete=django.db.models.deletion.SET_DEFAULT, to='kulupulang.theme'),
+            model_name="user",
+            name="theme",
+            field=models.ForeignKey(
+                default=1,
+                help_text="the theme you want to display the site in",
+                on_delete=django.db.models.deletion.SET_DEFAULT,
+                to="kulupulang.theme",
+            ),
         ),
     ]
